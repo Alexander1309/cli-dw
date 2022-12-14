6339feba522a549ad17f3cff26a7846be93f9a7a
Alexander1309 <alexanderperez4893@gmail.com>
2022-12-14 01:05:04 -0600

    Dowloader version 2.0

--- dw.py
+++ dw.py
@@ -8,130 +8,74 @@ import moviepy.editor as mp
 import pafy
 from youtubesearchpython import VideosSearch
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--url', '-u')
-parser.add_argument('--names', '-n')
-parser.add_argument('--format', '-f')
-parser.add_argument('--path-dest', '-pd')
-args = list(vars(parser.parse_args()).values())
+class Dowloader:
+  def __init__(self):
+    self.url_file = ""
+    self.name_file = ""
+    self.path_file = ""
+    self.format_file = ""
+    self.path_file_dest = ""
 
-count = 0
-name = ""
-errors = []
+    self.unloader_extensions = {
+      "mp3": "m4a",
+      "mp4": "mp4"
+    }
 
+    self.count_errors = 0
+    self.message_errors = []
+    self.path_files_errors = []
 
-def write_log_error(msg):
-    file_log = open('./logs.txt', 'w')
-    file_log.writelines(msg)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', '-u')
+    parser.add_argument('--file', '-fi')
+    parser.add_argument('--format', '-ft')
+    parser.add_argument('--path-dest', '-pd')
+    self.args = list(vars(parser.parse_args()).values())
+
+  def write_log_error(self):
+    file_log = open('./songs_not_downloaded.txt', 'w')
+    file_log.writelines(self.message_errors)
     file_log.close()
 
-
-def move_file(name, path):
-    shutil.move(abspath('./{0}'.format((name))), path)
-
-
-def dowload_audio(url, path, format):
-    global count, name, errors
-    try:
-        system('cls')
-        pafyNew = pafy.new(url)
-        parser_name = " ".join(
-            re.sub(r"[^a-zA-Z0-9]", " ", pafyNew.title).split())
-        name = parser_name
-        name_audio = f'{parser_name}.{format}'
-        path_audio = abspath(f'{parser_name}.m4a')
-        if isfile(join(path, name_audio)):
-            print(
-                f'\nFile with url: {url} and name: {name_audio} already exist')
-            return
-        print(f'\nDownloading url: {url} name: {name_audio}')
-        video = pafyNew.getbestaudio(preftype='m4a')
-        video.download(f'{parser_name}.m4a')
-        audio_m4a = mp.AudioFileClip(path_audio)
-        audio_m4a.write_audiofile(name_audio, bitrate="320k")
-        audio_m4a.close()
-        remove(path_audio)
-        move_file(name_audio, path)
-        system('cls')
-        print("Download completed...")
-        count = 0
-    except:
-        count += 1
-
-        if count == 4:
-            count = 0
-            system('cls')
-            errors.append(
-                f'File with Name: {name}, Url: {url} and Format: {format} could not be downloaded \n')
-            return
-        system('cls')
-        print('Dowload error trying again...')
-        dowload_audio(url, path, format)
-
-
-def dowload_mp4(url, path, format):
-    global count, name, errors
-    try:
-        system('cls')
-        pafyNew = pafy.new(url)
-        nameFile = f'{pafyNew.title}.mp4'
-        name = nameFile
-        if isfile(join(path, nameFile)):
-            print(f'\nFile with url: {url} and name: {nameFile} already exist')
-            return
-        print(f'\nDownloading url: {url} name: {nameFile}')
-        video = pafyNew.getbest(preftype='mp4')
-        video.download()
-        move_file(nameFile, path)
-        system('cls')
-        print("Download completed...")
-        count = 0
-    except:
-        count += 1
-
-        if count == 4:
-            count = 0
-            system('cls')
-            errors.append(
-                f'File with Name: {name}, Url: {url} and Format: {format} could not be downloaded \n')
-            return
-        system('cls')
-        print('Dowload error trying again...')
-        dowload_mp4(url, path, format)
-
-
-formats = {
-    'audio': dowload_audio,
-    'mp4': dowload_mp4
-}
-
-
-def read_file(path):
+  def read_file(self, path):
     readData = []
     with open(path, 'r') as file:
-        readFile = file.readlines()
-        for _, read in enumerate(readFile):
-            readData.append(read.strip('\n').strip('\t'))
+      readFile = file.readlines()
+      for _, read in enumerate(readFile):
+        readData.append(read.strip('\n').strip('\t'))
     return readData
 
+  def delete_files(self):
+    for _, path_file_error in enumerate(self.path_files_errors):
+      remove(path_file_error)
+  
+  def move_download(self):
+    if self.format_file == "mp3":
+      remove(self.path_unloader)
+    shutil.move(abspath(f'./{self.name_unloader}'), self.path_file_dest)
 
-def validations(args):
-    print('Validating...')
-    url, file_names, format, path_dest = args
-    if not format in {'mp3', 'ogg', 'wav', 'mp4'}:
-        raise OSError(
-            'format is invalid... formats valids: mp3, wav, ogg, mp4')
+  def convert_format(self):
+    audio_convert = mp.AudioFileClip(self.path_unloader)
+    audio_convert.write_audiofile(self.name_unloader, bitrate="320k")
+    audio_convert.close()
+
+  def validate_args(self):
+    print('Validating args...\n')
+    url, file_names, format, path_dest = self.args
+
+      
+    if not format in {'mp3', 'mp4'}:
+      raise OSError('Format is invalid... formats valids: mp3, mp4')
 
     if path_dest == None:
-        raise OSError(
-            'Destination path cannot be empty please add the path with the command --path-dest or -pd')
-
+      raise OSError('Destination path cannot be empty please add the path with the command --path-dest or -pd')
+    
     if file_names != None and url == None and not isfile(file_names):
         raise OSError("""The path of the names file is invalid or does not exist please
     check the path or the name file format should be .txt
       
     example:
-      -fu C:\\Users\\username\\names.txt
+      -fi C:\\Users\\username\\names.txt
 
     data of file
       name 1
@@ -140,35 +84,83 @@ def validations(args):
   """)
 
     if not isdir(path_dest):
-        mkdir(path_dest)
+      mkdir(path_dest)
 
+  def unloader(self):
+    try:
+      pafyNew = pafy.new(self.url_file)
+      self.name_file = " ".join(re.sub(r"[^a-zA-Z0-9]", " ", pafyNew.title).split())
+      self.name_unloader = f'{self.name_file}.{self.format_file}'
+      self.path_unloader = f'{self.name_file}.{self.unloader_format}'
 
-def get_urls(data):
+      self.dowloader_methods = {
+        "mp3": pafyNew.getbestaudio(preftype=f'{self.unloader_format}'),
+        "mp4": pafyNew.getbest(preftype=f'{self.unloader_format}'),
+      }
+
+      if isfile(join(self.path_file_dest, self.name_unloader)):
+        print(f'\nFile with url: {self.url_file} and name: {self.name_file} already exist')
+        return
+      print(f"""\n===  Downloading  ===
+> Name: {self.name_file}
+> Format: {self.format_file}
+> Url: {self.url_file} 
+> Path_dest: {self.path_file_dest} \n
+      """)
+      unloader = self.dowloader_methods[self.format_file]
+      unloader.download(f'{self.name_file}.{self.unloader_format}')
+      if self.format_file == "mp3":
+        self.convert_format()
+      self.move_download()
+      print("\nDownload completed...")
+      system("cls")
+      self.count_errors = 0
+    except:
+      self.count_errors += 1
+
+      if self.count_errors == 4:
+        self.count_errors = 0
+        self.path_files_errors.append(f'{self.path_unloader}.part')
+        system('cls')
+        self.message_errors.append(f"""=== File could not be downloaded ===
+> Name: {self.name_file}
+> Url: {self.url_file}
+> Format: {self.format_file} \n\n""")
+        return
+      system('cls')
+      print(f'Error downloading trying again attempt {self.count_errors} of 3')
+      self.unloader()
+
+  def get_urls(self, data):
     urls = []
     for _, n in enumerate(data):
-        print(f'Opting url of the song: {n}')
-        url = VideosSearch(n, limit=1)
-        urls.append(url.result()['result'][0]['link'])
+      print(f'Opting url of the song: {n}')
+      url = VideosSearch(n, limit=1)
+      urls.append(url.result()['result'][0]['link'])
     return urls
 
+  def main(self):
+    self.validate_args()
+    url, file_names, format, path_dest = self.args
 
-def main():
-    validations(args)
-    url, file_names, extension, path_dest = args
-    format = 'mp4'
-    if extension in {'mp3', 'ogg', 'wav'}:
-        format = 'audio'
-    if url != None and file_names == None:
-        formats[format](url, path_dest, extension)
-    elif file_names != None and url == None:
-        names = read_file(file_names)
-        urls = get_urls(names)
-        for i in range(len(urls)):
-            formats[format](urls[i], path_dest, extension)
-        else:
-            if len(errors) != 0:
-              write_log_error(errors)
+    self.format_file = format
+    self.path_file_dest = path_dest
+    self.unloader_format = self.unloader_extensions[self.format_file]
 
+    if file_names != None and url == None:
+      names = self.read_file(file_names)
+      urls = self.get_urls(names)
+      for i in range(len(urls)):
+        self.url_file = urls[i]
+        self.unloader()
+      else:
+          if len(self.message_errors) != 0:
+            self.delete_files()
+            self.write_log_error()
+    else:
+      self.url_file = url
+      self.unloader()
 
 if __name__ == '__main__':
-    main()
+  dw = Dowloader()
+  dw.main()
